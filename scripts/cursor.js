7708fb84b1b7226f6cb6d437923cacde423627e6
fifty7north <nathan.moir@outlook.com>
2024-03-12 14:37:20 +0000

    cursor trail

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -1,21 +1,39 @@
-const cursor = document.getElementById("cursor");
 const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".cursor-circle");
+const cursor = document.querySelector(".cursor");
 
-cursor.style.left = "0";
-cursor.style.left = "0";
+circles.forEach((circle) => {
+  circle.x = "0";
+  circle.y = "0";
+});
 
 window.addEventListener("mousemove", (e) => {
   coords.x = e.clientX;
   coords.y = e.clientY;
-
-  cursor.style.left = coords.x - 16 + "px";
-  cursor.style.top = coords.y - 16 + "px";
 });
 
-window.addEventListener("mouseover", () => {
-  cursor.style.opacity = "1";
-});
+function animateCircles() {
+  let x = coords.x;
+  let y = coords.y;
 
-window.addEventListener("mouseout", () => {
-  cursor.style.opacity = "0";
-});
+  cursor.style.left = x;
+  cursor.style.top = y;
+
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+
+    circle.style.scale = (circles.length - index) / circles.length;
+
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
